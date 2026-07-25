6d1e141d49fd424b0adb419d2ba0bee506d9dbcd
Quentin <quentin.mari0409@gmail.com>
2026-07-25 20:25:43 +0200

    🐛 Create the layer seeders five providers referenced without them existing

---
+++ functional/identification/database/seeders/IdentificationSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Functional\Identification\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class IdentificationSeeder extends Seeder
+{
+    /**
+     * Nothing to seed until the identification layer is built.
+     */
+    public function run(): void {}
+}
